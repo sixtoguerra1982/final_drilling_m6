242ae8000df1c02ce5ef23e6b12a6e1c03d371a0
= <=>
2023-07-20 20:35:01 -0400

    Comic add front

--- www/assets/js/index.js
+++ www/assets/js/index.js
@@ -7,6 +7,14 @@ window.addEventListener('load',
 
             let data = await chargeAnime();
             drawPostIt(data);
+
+
+            let btnNew = document.getElementById("btn_new");
+            btnNew.addEventListener('click', (event) => {
+                event.preventDefault();
+                newForm(event);
+            })
+
         }, 500);
     }, false);
 
@@ -21,7 +29,7 @@ function chargeAnime() {
         }
         
     });
-}
+};
 
 function drawPostIt(data) {
     const main = document.getElementById("main");
@@ -50,6 +58,94 @@ function drawPostIt(data) {
             </div>
         </div>
         `
-        main.innerHTML += draw
-    }
+        main.innerHTML += draw;
+    };
+};
+
+
+function newForm(event) {
+    
+    const modals = document.querySelectorAll(".modal");
+    const modalCloseButtons = document.querySelectorAll(".modal-close");
+    
+    toggleModal(event.currentTarget.getAttribute("data-modal-target"));
+    
+    modalCloseButtons.forEach(elem => {
+      elem.addEventListener("click", event => toggleModal(event.currentTarget.closest(".modal").id));
+    });
+    modals.forEach(elem => {
+      elem.addEventListener("click", event => {
+        if(event.currentTarget === event.target) toggleModal(event.currentTarget.id);
+      });
+    });  
+}
+
+// Close Modal with "Esc"...
+document.addEventListener("keydown", event => {
+  if(event.keyCode === 27 && document.querySelector(".modal.modal-show")) {
+    toggleModal(document.querySelector(".modal.modal-show").id);
+  }
+});
+
+function toggleModal(modalId) {
+  const modal = document.getElementById(modalId);
+
+  if(getComputedStyle(modal).display==="flex") { // alternatively: if(modal.classList.contains("modal-show"))
+    modal.classList.add("modal-hide");
+    setTimeout(() => {
+      document.body.style.overflow = "initial";
+      modal.classList.remove("modal-show", "modal-hide");
+      modal.style.display = "none";      
+    }, 200);
+  }
+  else {
+    document.body.style.overflow = "hidden";
+    modal.style.display = "flex";
+    modal.classList.add("modal-show");
+  }
+}
+
+function guardarComic() {
+    // Obtener los valores del formulario
+    let nombre = document.getElementById("nombre").value;
+    let genero = document.getElementById("genero").value;
+    let anio = document.getElementById("anio").value;
+    let autor = document.getElementById("autor").value;
+
+    let comic = {
+        nombre: nombre,
+        genero: genero,
+        año: anio,
+        autor: autor
+    };
+    // Realizar la solicitud POST con Fetch
+    fetch("/api/v1/anime", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(comic)
+    })
+    .then(response => response.json())
+    .then(data => {
+
+        alert("Cómic guardado correctamente:\n\nNombre: " + comic.nombre + "\nGénero: " + comic.genero + "\nAño: " + comic.año + "\nAutor: " + comic.autor);
+        toggleModal(document.querySelector(".modal.modal-show").id);
+
+        let obj = {
+            "1": comic
+        };
+
+        drawPostIt(obj);
+
+        document.getElementById("comicForm").reset();
+
+        console.log(data);
+    })
+    .catch(error => {
+        console.error("Error al realizar la solicitud:", error);
+    });
+    
+    // Devolver false para evitar que el formulario se envíe al servidor
+    return false;
 }
